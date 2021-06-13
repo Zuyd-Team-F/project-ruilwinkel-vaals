6f833e37acf1487e06f8d04d97db565c6a7d52f0
Tim <tim.crounse@gmail.com>
2021-06-13 21:15:58 +0200

    Initial Commit

---
+++ RuilwinkelVaals.WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RuilwinkelVaals.WebApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuilwinkelVaals.WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ RuilwinkelVaals.WebApp/Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuilwinkelVaals.WebApp.Data
+{
+    public class DbInitializer
+    {
+        public static void Init(ApplicationDbContext context)
+        {
+            context.Database.Migrate();
+        }
+    }
+}
