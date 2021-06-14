1df60da375991427b3e99e776a4cd26f7408ab8e
Robby <RobbyyICT@gmail.com>
2021-06-14 16:05:05 +0200

    Create CheckEvent.cs

---
+++ RuilwinkelVaals.WebApp/Classes/CheckEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuilwinkelVaals.WebApp.Classes
+{
+    public static class CheckEvent
+    {
+        // Check if string is empty.
+        public static Boolean isStringEmpty(String input)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
