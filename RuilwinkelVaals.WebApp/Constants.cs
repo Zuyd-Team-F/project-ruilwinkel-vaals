e36ea303674d8aeec36d62cfd538fa98007498ab
Tim <tim.crounse@gmail.com>
2021-06-15 16:33:06 +0200

    Added constants

---
+++ RuilwinkelVaals.WebApp/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuilwinkelVaals.WebApp
+{
+    public static class Constants
+    {
+        public enum Roles
+        { 
+            ADMIN,
+            MEDEWERKER,
+            KLANT,
+            BUSINESS
+        }
+    }
+}
