16e87dba35dee505a5fe8043a48630f123d36ca6
DeanV1999 <71329845+DeanV1999@users.noreply.github.com>
2021-06-21 14:18:53 +0200

    Opzet automatische tests

--- RuilwinkelVaals.Tests/UnitTest1.cs
+++ RuilwinkelVaals.Tests/UnitTest1.cs
@@ -6,9 +6,14 @@ namespace RuilwinkelVaals.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void PassingTest()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
 
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
