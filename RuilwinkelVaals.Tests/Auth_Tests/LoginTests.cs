607634f466f95155cc2a00234e30aff300157ca7
Yoriaantje <1904507hanckmann@zuyd.nl>
2021-06-23 15:21:22 +0200

    Create LoginTests.cs

---
+++ RuilwinkelVaals.Tests/Auth_Tests/LoginTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RuilwinkelVaals.Tests.Auth_Tests
+{
+    public class LoginTests
+    {
+        [Fact]
+        public void TestingTest()
+        {
+            // This will succeed
+            Assert.Equal("test", "test");
+        }
+    }
+}
