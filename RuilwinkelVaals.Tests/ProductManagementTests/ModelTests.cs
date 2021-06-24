cf045744a228614083e97b9b859cf151bb6b7ecd
Stygall <59049395+Stygall@users.noreply.github.com>
2021-06-24 15:08:25 +0200

    tweak to modeltest to remove duplicate objects

--- RuilwinkelVaals.Tests/ProductManagementTests/ModelTests.cs
+++ RuilwinkelVaals.Tests/ProductManagementTests/ModelTests.cs
@@ -34,37 +34,16 @@ namespace RuilwinkelVaals.Tests.ProductManagementTests
             Assert.Equal(status, item.Status);
             Assert.Equal("test test", item.Description);
             Assert.Equal(123, item.CreditValue);
-        }
-
-        [Fact]
-        public void ConditionModelTest()
-        {
-            //making an instance of condition
-            Condition condition = new Condition() { Name = "Zeerslecht" };
 
             //testing to see if the input values remain correct when creating the object 
             Assert.Equal("Zeerslecht", condition.Name);
-        }
-
-        [Fact]
-        public void CategoryModelTest()
-        {
-            //making an instance of category
-            Category category = new Category() { Name = "Electronica" };
 
             //testing to see if the input values remain correct when creating the object 
             Assert.Equal("Electronica", category.Name);
-        }
-
-        [Fact]
-        public void StatusnModelTest()
-        {
-            //making an instance of status
-            Status status = new Status() { Name = "Voorradig" };
 
             //testing to see if the input values remain correct when creating the object 
             Assert.Equal("Voorradig", status.Name);
         }
-#endregion
+        #endregion
     }
 }
