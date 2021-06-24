46e271cf92444879833c3fc459b3a72bc8f97fef
TimCRN <tim.crounse@gmail.com>
2021-06-25 01:29:06 +0200

    Added UserIndexViewModel

---
+++ RuilwinkelVaals.WebApp/ViewModels/Users/UserIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RuilwinkelVaals.WebApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuilwinkelVaals.WebApp.ViewModels.Users
+{
+    public class UserIndexViewModel
+    {        
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Role { get; set; }
+    }
+}
