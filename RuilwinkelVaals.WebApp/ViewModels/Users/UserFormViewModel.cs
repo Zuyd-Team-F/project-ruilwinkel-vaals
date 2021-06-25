ad8b45a063a9de48d734467cfd7e50c0a229a272
TimCRN <tim.crounse@gmail.com>
2021-06-25 17:45:57 +0200

    Model dutchify

--- RuilwinkelVaals.WebApp/ViewModels/Users/UserFormViewModel.cs
+++ RuilwinkelVaals.WebApp/ViewModels/Users/UserFormViewModel.cs
@@ -8,38 +8,51 @@ namespace RuilwinkelVaals.WebApp.ViewModels.Users
     {
         public int Id { get; set; }
 
+        [Display(Name = "Bedrijf")]
         public int? BusinessId { get; set; }
 
+        [Display(Name = "Rol")]
         public int RoleId { get; set; }
 
         public SelectList Businesses { get; set; }
 
         public SelectList Roles { get; set; }
 
+        [Display(Name = "Voornaam")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Achternaam")]
         public string LastName { get; set; }
 
+        [Display(Name = "Wachtwoord")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Geboorte datum")]
         public DateTime DateOfBirth { get; set; }
 
+        [Display(Name = "Straat")]
         public string Street { get; set; }
 
+        [Display(Name = "Huisnummer")]
         public int StreetNumber { get; set; }
 
+        [Display(Name = "Straat toevoeging")]
         public string StreetAdd { get; set; }
 
+        [Display(Name = "Postcode")]
         public string PostalCode { get; set; }
 
+        [Display(Name = "Stad")]
         public string City { get; set; }
 
+        [Display(Name = "E-Mail")]
         public string Email { get; set; }
 
+        [Display(Name = "Telefoon")]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Balans")]
         public int Balance { get; set; }
-
     }
 }
--- RuilwinkelVaals.WebApp/ViewModels/Users/UserInfoViewModel.cs
+++ RuilwinkelVaals.WebApp/ViewModels/Users/UserInfoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RuilwinkelVaals.WebApp.ViewModels.Users
 {
@@ -7,30 +8,43 @@ namespace RuilwinkelVaals.WebApp.ViewModels.Users
     {
         public int Id { get; set; }
 
+        [Display(Name = "Bedrijf")]
         public string Business { get; set; }
 
+        [Display(Name = "Rol")]
         public string Role { get; set; }
 
+        [Display(Name = "Voornaam")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Achternaam")]
         public string LastName { get; set; }
 
+        [Display(Name = "Geboorte datum")]
         public DateTime DateOfBirth { get; set; }
 
+        [Display(Name = "Straat")]
         public string Street { get; set; }
 
+        [Display(Name = "Huisnummer")]
         public int StreetNumber { get; set; }
 
+        [Display(Name = "Straat toevoeging")]
         public string StreetAdd { get; set; }
 
+        [Display(Name = "Postcode")]
         public string PostalCode { get; set; }
 
+        [Display(Name = "Stad")]
         public string City { get; set; }
 
+        [Display(Name = "E-Mail")]
         public string Email { get; set; }
 
+        [Display(Name = "Telefoon")]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Balans")]
         public int Balance { get; set; }
 
         public bool Blacklist { get; set; }
