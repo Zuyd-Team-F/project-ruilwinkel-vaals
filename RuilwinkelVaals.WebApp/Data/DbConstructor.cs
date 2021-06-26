e1164f026d826d15bd8f7eb58774d27802c305b5
TimCRN <tim.crounse@gmail.com>
2021-06-27 00:36:39 +0200

    User added to constructor

--- RuilwinkelVaals.WebApp/Data/DbConstructor.cs
+++ RuilwinkelVaals.WebApp/Data/DbConstructor.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace RuilwinkelVaals.WebApp.Data
 {
@@ -20,21 +21,41 @@ namespace RuilwinkelVaals.WebApp.Data
         private readonly IWebHostEnvironment _env;
         private readonly ApplicationDbContext _context;
         private readonly ILogger _logger;
+        private readonly IConfiguration _config;
 
-        public DbConstructor(IWebHostEnvironment environment, ApplicationDbContext context, ILogger<DbConstructor> logger)
+        public DbConstructor(IWebHostEnvironment environment, ApplicationDbContext context, ILogger<DbConstructor> logger, IConfiguration configuration)
         {
             _env = environment;
             _context = context;
             _logger = logger;
+            _config = configuration;
         }
 
         public async Task Init()
         {
             if(_env.IsDevelopment())
             {
+                UserData devUser = new()
+                {
+                    FirstName = "Developer",
+                    LastName = "Zuyd",
+                    UserName = _config.GetSection("DevCredentials")["User"],
+                    Email = _config.GetSection("DevCredentials")["User"],
+                    City = "Dev City",
+                    EmailConfirmed = true,
+                    PostalCode = "6666TE",
+                    Street = "Test Avenue",
+                    StreetNumber = 1,
+                    PhoneNumber = "123456789",
+                    Balance = 0
+                };
+
+                devUser.PasswordHash = new PasswordHasher<UserData>()
+                    .HashPassword(devUser, _config.GetSection("DevCredentials")["Password"]);
+
                 _context.Database.EnsureDeleted();
                 _context.Database.Migrate();
-                await DbSeeder.Init(_context);
+                await DbSeeder.Init(_context, _env, devUser);
             }
             else
             {
