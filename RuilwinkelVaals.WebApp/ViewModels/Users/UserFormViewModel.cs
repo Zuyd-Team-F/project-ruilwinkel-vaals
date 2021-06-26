5523b671afbf26415b53353bee5861a104ef0845
TimCRN <tim.crounse@gmail.com>
2021-06-26 12:53:08 +0200

    Validation added

--- RuilwinkelVaals.WebApp/ViewModels/Users/UserFormViewModel.cs
+++ RuilwinkelVaals.WebApp/ViewModels/Users/UserFormViewModel.cs
@@ -11,6 +11,7 @@ namespace RuilwinkelVaals.WebApp.ViewModels.Users
         [Display(Name = "Bedrijf")]
         public int? BusinessId { get; set; }
 
+        [Required]
         [Display(Name = "Rol")]
         public int RoleId { get; set; }
 
@@ -18,34 +19,44 @@ namespace RuilwinkelVaals.WebApp.ViewModels.Users
 
         public SelectList Roles { get; set; }
 
+        [Required]
         [Display(Name = "Voornaam")]
         public string FirstName { get; set; }
 
+        [Required]
         [Display(Name = "Achternaam")]
         public string LastName { get; set; }
 
+        [Required]
         [Display(Name = "Wachtwoord")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required]
         [Display(Name = "Geboorte datum")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
+        [Required]
         [Display(Name = "Straat")]
         public string Street { get; set; }
 
+        [Required]
         [Display(Name = "Huisnummer")]
         public int StreetNumber { get; set; }
 
         [Display(Name = "Straat toevoeging")]
         public string StreetAdd { get; set; }
 
+        [Required]
         [Display(Name = "Postcode")]
         public string PostalCode { get; set; }
 
+        [Required]
         [Display(Name = "Stad")]
         public string City { get; set; }
 
+        [Required]
         [Display(Name = "E-Mail")]
         public string Email { get; set; }
 
