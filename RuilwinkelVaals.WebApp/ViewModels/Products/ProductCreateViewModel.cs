a517d58434fa11a32f66bed8dc14041ea3b8296d
TimCRN <tim.crounse@gmail.com>
2021-06-29 21:21:12 +0200

    VoewModel created for Product

---
+++ RuilwinkelVaals.WebApp/ViewModels/Products/ProductCreateViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuilwinkelVaals.WebApp.ViewModels.Products
+{
+    public class ProductCreateViewModel
+    {
+        [Required(ErrorMessage = "Kies een categorie")]
+        [Display(Name = "Categorie")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Kies de kwaliteit van het product")]
+        [Display(Name = "Kwaliteit")]
+        public int ConditionId { get; set; }
+
+        [Required(ErrorMessage = "Geef de huidige status van het product")]
+        [Display(Name = "Status")]
+        public int StatusId { get; set; }
+
+        [MaxLength(24)]
+        [Display(Name = "Merk")]
+        public String Brand { get; set; }
+
+        [Display(Name = "Beschrijving")]
+        public String Description { get; set; }
+
+        [Required(ErrorMessage = "Geef de naam van het product")]
+        [Display(Name = "Naam")]
+        [MaxLength(56)]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Geef de waarde van het product")]
+        [Display(Name = "Creditwaarde")]
+        public int CreditValue { get; set; }
+
+        [Display(Name = "Product Foto")]
+        public IFormFile Image { get; set; }
+
+        public SelectList Categories { get; set; }
+        public SelectList Conditions { get; set; }
+        public SelectList Statusses { get; set; }
+    }
+}
