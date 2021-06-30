d4cf1c52b678fd05d7a5567ef5ece437c77a517d
TimCRN <tim.crounse@gmail.com>
2021-06-30 15:16:17 +0200

    ChangeProfilePhoto added

---
+++ RuilwinkelVaals.WebApp/Areas/Identity/Pages/Account/Manage/ChangeProfilePhoto.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RuilwinkelVaals.WebApp.Areas.Identity.Pages.Account.Manage
+{
+    public class ChangeProfilePhotoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
