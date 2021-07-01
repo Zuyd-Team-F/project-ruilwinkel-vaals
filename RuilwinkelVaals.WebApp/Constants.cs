359a290f4f347b4cdde2e1c18f43c435a1b509ae
TimCRN <tim.crounse@gmail.com>
2021-07-01 16:16:37 +0200

    constants fix

--- RuilwinkelVaals.WebApp/Constants.cs
+++ RuilwinkelVaals.WebApp/Constants.cs
@@ -14,13 +14,11 @@ namespace RuilwinkelVaals.WebApp
 
         public enum Conditions
         {
-            [Display(Name = "Zeer goed")] 
-            ZeerGoed,
+            Zeer_Goed,
             Goed,
             Neutraal,
             Slecht,
-            [Display(Name = "Zeer slecht")] 
-            ZeerSlecht
+            Zeer_Slecht
         };
 
         public enum Statuses
