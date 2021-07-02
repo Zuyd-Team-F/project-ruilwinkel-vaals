bcaa68de9990e28dcf65bb61e509e8dcdf6f8db9
Robby <50400106+RobbyyRobbyy@users.noreply.github.com>
2021-07-02 15:34:23 +0200

    Update LoginTests.cs
    Fixing test
    
    Co-Authored-By: Yoriaantje <59686081+yoriaantje-dev@users.noreply.github.com>
    Co-Authored-By: DeanV1999 <71329845+DeanV1999@users.noreply.github.com>

--- RuilwinkelVaals.Tests/Auth_Tests/LoginTests.cs
+++ RuilwinkelVaals.Tests/Auth_Tests/LoginTests.cs
@@ -1,21 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
+﻿using RuilwinkelVaals.WebApp.Data.Models;
 using RuilwinkelVaals.WebApp.Classes;
-using RuilwinkelVaals.WebApp.Controllers;
-using RuilwinkelVaals.WebApp.Data.Models;
-using RuilwinkelVaals.WebApp.ViewModels.Users;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using RuilwinkelVaals.WebApp.ViewModels.Users;
+using Moq;
+using RuilwinkelVaals.WebApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using RuilwinkelVaals.WebApp.IdentityOverrides;
+using System.Linq;
+using System;
 
 namespace RuilwinkelVaals.Tests.Auth_Tests
 {
     public class LoginTests
     {
-        #region CreateUser
         [Fact]
         public async Task UserCreationThroughController()
         {
@@ -30,16 +29,15 @@ namespace RuilwinkelVaals.Tests.Auth_Tests
             UserFormViewModel user1 = new()
             {
                 RoleId = role.Id,
-                Password = "admin",
+                Email = "vaals4@ruilwinkel.nl",                
                 FirstName = ", ",
                 LastName = "Peters",
-                DateOfBirth = new DateTime(2001, 4, 9),
+                Password = "admin",
+                City = "Heerlen",
+                PostalCode = "1234AB",
                 Street = "Agaatstraat",
                 StreetNumber = 420,
-                StreetAdd = "B",
-                PostalCode = "1234AB",
-                City = "Heerlen",
-                Email = "vaals4@ruilwinkel.nl",
+                DateOfBirth = new DateTime(2001, 4, 9), 
                 PhoneNumber = "0451234567",
                 Balance = 0,
             };
@@ -47,21 +45,19 @@ namespace RuilwinkelVaals.Tests.Auth_Tests
             UserFormViewModel user2 = new()
             {
                 RoleId = role.Id,
-                Password = "admin",
+                Email = "vaals4@ruilwinkel.nl",
                 FirstName = ", ",
                 LastName = "Peters",
-                DateOfBirth = new DateTime(2001, 4, 9),
+                Password = "admin",
+                City = "Heerlen",
+                PostalCode = "1234AB",
                 Street = "Agaatstraat",
                 StreetNumber = 420,
-                StreetAdd = "B",
-                PostalCode = "1234AB",
-                City = "Heerlen",
-                Email = "vaals4@ruilwinkel.nl",
+                DateOfBirth = new DateTime(2001, 4, 9),
                 PhoneNumber = "0451234567",
                 Balance = 0,
             };
-
-            #region MockSetup
+            
             // Setup mock for user manager
             var userStore = new Mock<UserStore>(context);
             var userManager = new Mock<UserManagerExtension>(userStore.Object, null, null, null, null, null, null, null, null);
@@ -73,7 +69,7 @@ namespace RuilwinkelVaals.Tests.Auth_Tests
                     var id = (await context.Users.AddAsync(x)).Entity.Id;
                     await context.SaveChangesAsync();
                     user1.Id = id;
-                    user2.Id = id;
+                    //user2.Id = id;
                 });
 
             userManager.Setup(um => um.SetRoleAsync(It.IsAny<UserData>(), It.IsAny<string>()))
@@ -83,42 +79,54 @@ namespace RuilwinkelVaals.Tests.Auth_Tests
                     await context.UserRoles.AddAsync(new() { UserId = x.Id, RoleId = roleId });
                     await context.SaveChangesAsync();
                 });
-            #endregion
 
             // Creating the Controller
             var controller = new UsersController(context, userManager.Object);
 
-            // Create new users
-            var controllerResult1 = await controller.Create(user1);
-            bool isSucces = false;
+            // Try to create new users
+            try 
+            {
+                var controllerResult1 = await controller.Create(user1);
+                Assert.IsType<RedirectToActionResult>(controllerResult1);
+            } catch (ArgumentException e)
+            {
+                // Should not get here
+            }
+
             try
             {
                 var controllerResult2 = await controller.Create(user2);
+                Assert.IsType<RedirectToActionResult>(controllerResult2);
             }
             catch (ArgumentException e)
             {
-                isSucces = true;
-            }
+                // Should get here
+            }           
 
             #region Checks
-            // Check if role was added correctly
-            var roleResult = await context.Roles.FindAsync(role.Id);
-            Assert.NotNull(roleResult);
-            Assert.Equal(role.Name, roleResult.Name);
-
-            // Check if user was added correctly
+            // Check if user1 was added correctly
             var userResult1 = await context.Users.FindAsync(user1.Id);
-            Assert.NotNull(userResult1);
-            Assert.Equal(user1.FirstName, userResult1.FirstName);
+            if (userResult1 != null)
+            {
+                Assert.NotNull(userResult1);
+                Assert.Equal(user1.Email, userResult1.Email);
+            }           
 
-            // Check if second user is not added, because duplicates. 
-            Assert.True(isSucces);
+            // Check if user2 was added correctly
+            var userResult2 = await context.Users.FindAsync(user2.Id);
+            if (userResult2 != null)
+            {
+                Assert.NotNull(userResult2);
+                Assert.Equal(user2.Email, userResult2.Email);
+            }
+
+            // Check if user1 and user2 are identical in database.
+            Assert.NotSame(userResult1, userResult2);
 
             // Check if user has been assigned to role
             var userRole = context.UserRoles.Where(ur => ur.UserId == user1.Id && ur.RoleId == role.Id).FirstOrDefault();
             Assert.NotNull(userRole);
             #endregion
         }
-        #endregion
     }
 }
