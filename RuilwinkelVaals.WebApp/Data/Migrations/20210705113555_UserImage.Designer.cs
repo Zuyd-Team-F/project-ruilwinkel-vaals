db6cddf05ebc831da3c8ef7f207397a604fccc83
TimCRN <tim.crounse@gmail.com>
2021-07-05 13:40:07 +0200

    UserData image property added

--- RuilwinkelVaals.WebApp/Data/Migrations/20210705113555_UserImage.Designer.cs
+++ RuilwinkelVaals.WebApp/Data/Migrations/20210705113555_UserImage.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RuilwinkelVaals.WebApp.Data;
 
 namespace RuilwinkelVaals.WebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210705113555_UserImage")]
+    partial class UserImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
